5a0e4419a2639b583138eee28fdecb88a392890a
Folaju Oyegbesan <fj@folaju.com>
2021-06-20 18:06:09 +0100

    update to scss, added icons

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,7 @@
 import * as React from "react"
 import { Link } from "gatsby"
-
-
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import '../utils/font-awesome';
 import Layout from "../components/layout"
 import Seo from "../components/seo"
 import SubscribeForm from "../components/SubscribeForm"
@@ -10,10 +10,10 @@ const IndexPage = () => (
   <Layout>
     <Seo title="Mefiye London" />
     <div className="container">
-      <div className="row">
+      <div className="row align-item-center justify-content-center">
         <div className="col-12"><h1>Mefiye London</h1></div>
       </div>
-      <div className="row">
+      <div className="row align-item-center justify-content-center">
         <div className="col-12 col-md-6">
           <p>"Mefiye" (pronouced meh&#9679;fyeh) ~ a play on the term for "home" in the Asante Twi language native to Ghana</p>
           <p>For us, <Link to="https://www.instagram.com/explore/tags/mefiyemeans/" target="_blank">#mefiyemeans</Link> sanctuary, luxury and warmth. With our collection of affordable luxury home fragrances and goods, we want to bring a piece of that feeling to you, too.</p>
@@ -22,6 +22,15 @@ const IndexPage = () => (
           <p>Enter your email address below to discover pur products and receive updates about our launch date.</p>
           <SubscribeForm/>
         </div>
+      </div>
+      <div className="row align-item-center justify-content-center">
+      <Link className="social-icons" to="https://www.instagram.com/mefiyelondon/" target="_blank">
+        <FontAwesomeIcon icon={['fab', 'instagram']} size="2x" />
+      </Link>
+      
+      <Link className="social-icons" to="https://twitter.com/MEFIYELONDON" target="_blank">
+        <FontAwesomeIcon icon={['fab', 'twitter']} size="2x" />
+      </Link>
       </div>
 
     </div>
