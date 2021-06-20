8396bd1459015ed2a4a7601b5bec43f6527c2987
Folaju Oyegbesan <fj@folaju.com>
2021-06-20 22:01:33 +0100

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -16,10 +16,10 @@ const IndexPage = () => (
       <div className="row align-item-center justify-content-center">
         <div className="col-12 col-md-6">
           <p className="text-align-center">"Mefiye" (pronouced meh&#9679;fyeh): a play on the term for "home" in the Asante Twi language native to Ghana.</p>
-          <p>For us, <Link to="https://www.instagram.com/explore/tags/mefiyemeans/" target="_blank">#mefiyemeans</Link> sanctuary, luxury and self-care. With our collection of affordable luxury home fragrances and goods, we want to bring a piece of that feeling to you, too.</p>
+          <p className="text-align-center">For us, <Link to="https://www.instagram.com/explore/tags/mefiyemeans/" target="_blank">#mefiyemeans</Link> sanctuary, luxury and self-care. With our collection of affordable luxury home fragrances and goods, we want to bring a piece of that feeling to you, too.</p>
         </div>
         <div className="col-12 col-md-6">
-          <p>Enter your email address below to discover our products and receive updates about our launch date.</p>
+          <p className="text-align-center">Enter your email address below to discover our products and receive updates about our launch date.</p>
           <SubscribeForm/>
         </div>
       </div>
